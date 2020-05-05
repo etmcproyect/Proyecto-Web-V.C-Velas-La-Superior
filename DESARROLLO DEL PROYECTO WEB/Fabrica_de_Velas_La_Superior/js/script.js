fa71deec857e508543352822d5daa69172228299
Christian <mxcat.xxtremxx@gmail.com>
2020-05-05 16:28:16 -0500

    Terminado seccion de noticia individual
    1.- se hizo responsive todas las paginas hasta el tamaño de 480 px 1360 px y 1920 px

--- DESARROLLO DEL PROYECTO WEB/Fabrica_de_Velas_La_Superior/js/script.js
+++ DESARROLLO DEL PROYECTO WEB/Fabrica_de_Velas_La_Superior/js/script.js
@@ -12,7 +12,9 @@ $('#carousel-card-testimonio').owlCarousel({
     responsive:{
         0:{
             items:1,
-            nav:true
+            nav:true,
+            mouseDrag: true,
+            touchDrag: true
         },
         600:{
             items:2,
@@ -54,6 +56,41 @@ $('#carousel-card-producto').owlCarousel({
         }
     }
 });
+
+$('#carousel-card-noticia').owlCarousel({
+    loop:true,
+    margin:0,
+    responsiveClass:true,
+    navElement: 'div',
+    navText: [],
+    navContainerClass: 'control-nav',
+    navClass: ['control-prev','control-next'],
+    dots: false,
+    mouseDrag: false,
+    touchDrag: false,
+    responsive:{
+        0:{
+            items:1,
+            nav:false,
+            mouseDrag: true,
+            touchDrag: true,
+        },
+        600:{
+            items:2,
+            nav:true,
+            mouseDrag: false,
+            touchDrag: false,
+        },
+        1000:{
+            items:2,
+            nav:true,
+            loop:true,
+            mouseDrag: false,
+            touchDrag: false,
+        }
+    }
+});
+
 $('#carousel-testimonio').carousel({
     interval:false
 })
