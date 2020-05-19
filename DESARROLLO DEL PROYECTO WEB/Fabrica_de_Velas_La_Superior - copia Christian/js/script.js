39dd725e4d0873d680cdf4233c9272fce43b729d
Christian <mxcat.xxtremxx@gmail.com>
2020-05-19 10:54:28 -0500

    subcopia de mi parte

---
+++ DESARROLLO DEL PROYECTO WEB/Fabrica_de_Velas_La_Superior - copia Christian/js/script.js
@@ -0,0 +1,104 @@
+$('#carousel-card-testimonio').owlCarousel({
+    loop:true,
+    margin:0,
+    responsiveClass:true,
+    navElement: 'div',
+    navText: [],
+    navContainerClass: 'control-nav',
+    navClass: ['control-prev','control-next'],
+    dots: false,
+    mouseDrag: false,
+    touchDrag: false,
+    responsive:{
+        0:{
+            items:1,
+            nav:true,
+            mouseDrag: true,
+            touchDrag: true
+        },
+        600:{
+            items:2,
+            nav:true
+        },
+        1000:{
+            items:2,
+            nav:true,
+            loop:true
+        }
+    }
+});
+$('#carousel-card-producto').owlCarousel({
+    loop:true,
+    margin:0,
+    responsiveClass:true,
+    navElement: 'div',
+    navText: [],
+    navContainerClass: 'control-nav',
+    navClass: ['control-prev','control-next'],
+    dots: false,
+    mouseDrag: false,
+    touchDrag: false,
+    responsive:{
+        0:{
+            items:1,
+            nav:false,
+            mouseDrag: true,
+            touchDrag: true,
+        },
+        600:{
+            items:2,
+            nav:true
+        },
+        1000:{
+            items:2,
+            nav:true,
+            loop:true
+        },
+        1200:{
+            items:3,
+            nav:true,
+            loop:true
+        }
+    }
+});
+
+$('#carousel-card-noticia').owlCarousel({
+    loop:true,
+    margin:0,
+    responsiveClass:true,
+    navElement: 'div',
+    navText: [],
+    navContainerClass: 'control-nav',
+    navClass: ['control-prev','control-next'],
+    dots: false,
+    mouseDrag: false,
+    touchDrag: false,
+    responsive:{
+        0:{
+            items:1,
+            nav:false,
+            mouseDrag: true,
+            touchDrag: true,
+        },
+        600:{
+            items:2,
+            nav:true,
+            mouseDrag: false,
+            touchDrag: false,
+        },
+        1000:{
+            items:2,
+            nav:true,
+            loop:true,
+            mouseDrag: false,
+            touchDrag: false,
+        }
+    }
+});
+
+$('#carousel-testimonio').carousel({
+    interval:false
+})
+$('#carousel-producto').carousel({
+    interval:false
+})
